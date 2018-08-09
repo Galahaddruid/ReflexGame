7f921442614321ee557829d24dd347fbe3ef2c0c
ttravi <ttravi@technomar.com.br>
2018-08-09 14:20:29 -0300

    Initial files load

---
+++ Assets/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BluePhoenix
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        [Range(0.1f, 5f)]
+        public float startDelay;
+
+        public GameObject fightSign;
+
+        [Range(0.1f, 5f)]
+        public float maxFightDelay;
+
+        bool started;
+
+        bool waiting;
+
+        float clickTime;
+        float fightTime;
+
+        enum GameState
+        {
+            Idle,
+            Started,
+            Ended
+        }
+
+        GameState gameState = GameState.Idle;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(gameState == GameState.Idle)
+            {
+                if(Input.anyKeyDown)
+                {
+                    gameState = GameState.Started;
+                    StartCoroutine(Fight(Random.Range(0.1f, maxFightDelay)));
+                }
+            }
+        }
+
+        IEnumerator Fight(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            fightSign.SetActive(true);
+        }
+    }
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+All notable changes to `Reflex Game` project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/pt-BR/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/lang/pt-BR/spec/v2.0.0.html).
+
+
+## unversioned
+
+### Added
+ * Initial files load
